9034740b4a16bc765b71b10fab90b6e16dca983c
Brauch25 <123976859+Brauch25@users.noreply.github.com>
2025-02-09 14:32:25 -0500

    Update happiness.py

--- feature-happiness/happiness.py
+++ feature-happiness/happiness.py
@@ -2,7 +2,8 @@ import pandas as pd
 
 # dataSet = pd.read_csv('https://www.kaggle.com/datasets/bhavikjikadara/emotions-dataset?resource=download')
 dataSet = pd.read_csv("feature-happiness/emotions.csv")
+output = 'happiness.csv'
 
-findingOnes=dataSet[dataSet['rank'==1]] #finding "1" = joy
+happinessSearch = dataSet[dataSet[rank']==1] #finding "1" = joy/happiness 
 
-print(findingOnes)
+print(findingOnes) 
