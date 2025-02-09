56788feff258326db065ea029cc088682ac35efb
Brauch25 <123976859+Brauch25@users.noreply.github.com>
2025-02-09 14:33:24 -0500

    Update happiness.py

--- feature-happiness/happiness.py
+++ feature-happiness/happiness.py
@@ -4,6 +4,6 @@ import pandas as pd
 dataSet = pd.read_csv("feature-happiness/emotions.csv")
 output = 'happiness.csv'
 
-happinessSearch = dataSet[dataSet[rank']==1] #finding "1" = joy/happiness 
+happinessSearch = dataSet[dataSet['rank']==1] #finding "1" = joy/happiness 
 
 print(happinessSearch) 
